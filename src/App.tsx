f540ce238f29bc4b8f41744b31a9e7b884f8e771
Worekit <worekit@gmail.com>
2023-05-24 17:10:33 +0200

    sm

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,11 @@ export default function App() {
   const [forecast2, setForecast2] = useState<forecastWeather>({ temp_c: 0, icon: "", date: "" });
   const [forecast3, setForecast3] = useState<forecastWeather>({ temp_c: 0, icon: "", date: "" });
 
-  const [city, setCity] = useState("Slupsk");
+  const [city, setCity] = useState("Tokyo");
 
   const [value, setValue] = useState<string | null>();
   const [inputValue, setInputValue] = useState<string>('');
 
-  const city_list = citiesData;
 
   const FetchData = () => {
     fetch(`http://api.weatherapi.com/v1/forecast.json?key=c890eee18037494394e111328230903&q=${city}&days=5&aqi=no&alerts=no`)
@@ -66,7 +65,6 @@ export default function App() {
 
   useEffect(() => {
     FetchData();
-
   }, []);
   
   return (
@@ -76,21 +74,13 @@ export default function App() {
 
         <div className='fav-cities' >
           
-          {/* <Autocomplete
-            value={value}
-            onChange={(event: any, newValue: string | null) => {setValue(newValue)}}
-            inputValue={inputValue}
-            onInputChange={(event, newInputValue) => {setInputValue(newInputValue)}}
-            id="controllable-states-demo"
-            options={citiesData.name}
-            renderInput={(params) => <TextField {...params} label="City" />}
-            className="autocomplete"
-          /> */}
+          <input type='text' placeholder='Search city' className='search-city' onChange={(e) => setCity(e.target.value)} />
+          <button className='search-button' onClick={FetchData} >Check</button>
           <CityBox name={city} temp={weather?.temp_c} icon={weather?.icon} />
         </div>
 
         <div className='weather-div wrap-flex' >
-          <div className='weather-div-names' >Poland, Słupsk</div>
+          <div className='weather-div-names' >{city}</div>
           <WeatherNow 
           temperature={weather?.temp_c} 
           wind={weather?.wind_kph} 
