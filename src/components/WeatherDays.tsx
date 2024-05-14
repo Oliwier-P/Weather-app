a6b520d3f5019f264fdd1a999b78dbcaf1efc730
Oliwier <worekit@gmail.com>
2024-05-14 18:09:34 +0200

    left side (current temperature, day, and future data)

--- src/components/WeatherDays.tsx
+++ src/components/WeatherDays.tsx
@@ -1,7 +1,32 @@
 export default function WeatherDays() {
+  const days = ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
   return (
     <>
-      <div className="weather-days"></div>
+      <div className="weather-days">
+        <div className="city-data">
+          <span className="city">Warsaw, Poland</span>
+          <span className="day">Monday 13 May</span>
+        </div>
+
+        <div className="current-data">
+          <span className="temperature">25°C</span>
+          <span className="description">Sunny</span>
+          <div className="imageD"></div>
+        </div>
+
+        <div className="future-data">
+          {days.map((day) => {
+            return (
+              <div className="future-day" key={day}>
+                <span className="future-day-text">{day}</span>
+                <span className="future-temperature">25°C</span>
+                <div className="future-imageD"></div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
